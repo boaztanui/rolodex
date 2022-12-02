efef1cae7dca3a1a910651188ccf24d553e2e853
boaztanui <boaztanui058@gmail.com>
2022-12-02 09:54:44 +0300

    Worked on handling the API and search input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,58 @@
-import logo from "./logo.svg";
+import { Component } from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Master Rolodex</h1>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(users);
+          }
+        )
+      );
+  }
+
+  render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            const searchField = event.target.value.toLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
